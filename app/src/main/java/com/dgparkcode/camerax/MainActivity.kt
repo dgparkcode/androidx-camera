af00a741b8e485733d366d9dc50b892af8e295bf
dgparkcode <dgpark.code@gmail.com>
2022-11-22 23:42:12 +0900

    feat: add camera permissions and functions
    * 카메라 권한을 승인받고 카메라 미리보기 화면을 구현합니다.

--- app/src/main/java/com/dgparkcode/camerax/MainActivity.kt
+++ app/src/main/java/com/dgparkcode/camerax/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.dgparkcode.camerax.databinding.ActivityMainBinding
@@ -19,15 +22,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 필요한 권한이 모두 승인된 경우 카메라 프리뷰를 시작합니다. 권한 승인이 안되어 있을 경우 권한을 요청합니다.
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
-    }
-
-    private fun startCamera() {
-
     }
 
     private fun allPermissionsGranted(): Boolean {
@@ -50,11 +50,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun startCamera() {
+        // 카메라의 수명 주기를 바인딩합니다.
+        // 수명 주기를 인식하므로 카메라를 열고 닫는 작업이 필요하지 않습니다.
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(
+            {
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                // 프리뷰를 초기화 합니다.
+                val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+
+                // 후면 카메라를 기본으로 설정합니다.
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                try {
+                    // 바인딩된 것이 있을 경우 해제하고, 프리뷰를 카메라 프로바이더에 바인드합니다.
+                    cameraProvider.unbindAll()
+
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            },
+
+            // 메인 스레드에서 실행되는 Executor 입니다.
+            ContextCompat.getMainExecutor(this)
+        )
+    }
+
     companion object {
         private const val REQUEST_CODE_PERMISSIONS = 10
+
+        // 필수 권한 목록입니다.
         private val REQUIRED_PERMISSIONS = mutableListOf(
             Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO
         ).apply {
+            // 안드로이드 9(P) 이하일 경우 외부 저장소 쓰기 권한을 추가합니다.
             if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                 add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
